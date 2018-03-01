b597b2316227d046020e6de0c370586aa7cdb780
ramamoorthy <ramamoorthy071@gmail.com>
2018-03-01 12:12:34 +0800

    Revert "edit 1"
    This reverts commit 42da13d58dff901a5bc4f8168825abbb4d47513a.

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 <?php
 echo "this is from git desktop - edited by atom editor.yes";
  ?>
+=======
+>>>>>>> parent of 42da13d... edit 1
 =======
 >>>>>>> parent of 42da13d... edit 1
 =======
